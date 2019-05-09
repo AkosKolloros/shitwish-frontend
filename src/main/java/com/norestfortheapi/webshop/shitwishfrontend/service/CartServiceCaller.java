8d416b29149bcddd7c4a4e482fa7f1d99d519b90
Halmai Mirella <halmaimirella@gmail.com>
2019-05-09 14:53:30 +0200

    methods to get data for cart

--- src/main/java/com/norestfortheapi/webshop/shitwishfrontend/service/CartServiceCaller.java
+++ src/main/java/com/norestfortheapi/webshop/shitwishfrontend/service/CartServiceCaller.java
@@ -1,6 +1,7 @@
 package com.norestfortheapi.webshop.shitwishfrontend.service;
 
 import com.norestfortheapi.webshop.shitwishfrontend.model.Cart;
+import com.norestfortheapi.webshop.shitwishfrontend.model.CartItem;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,22 +18,57 @@ public class CartServiceCaller {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Value("${url.base}")
-    private String baseUrl;
-
     @Value("${url.carts}")
-    private String cartUrl;
+    private String url;
 
-    public Cart getCart(long id) {
+    public Cart getCart(Long id) {
         try {
-            ResponseEntity<Cart> wishUserResponseEntity = restTemplate.exchange(baseUrl + cartUrl + "/" + id,
+            ResponseEntity<Cart> wishUserResponseEntity = restTemplate.exchange(url + id,
                     HttpMethod.GET
                     , null, new ParameterizedTypeReference<Cart>() {
                     });
             return wishUserResponseEntity.getBody();
         } catch (Exception e) {
             log.error(e.getMessage());
-            return null;
+            throw new ExecutionFailedException(e.getMessage());
         }
     }
+
+    public Cart addItemToCart(Long id, CartItem cartItem) {
+        try {
+            String postUrl = url + id + "/products";
+            ResponseEntity<Cart> wishUserResponseEntity = restTemplate.postForEntity(postUrl, cartItem, Cart.class);
+            return wishUserResponseEntity.getBody();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new ExecutionFailedException(e.getMessage());
+        }
+    }
+
+    public Cart createNewCart(Cart cart) {
+        try {
+            ResponseEntity<Cart> wishUserResponseEntity = restTemplate.postForEntity(url, cart, Cart.class);
+            return wishUserResponseEntity.getBody();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new ExecutionFailedException(e.getMessage());
+        }
+    }
+
+    public Cart deleteItemFromCart(Long id, Long productId) {
+        String deleteUrl = url + id + "/products/" + productId;
+        try {
+            ResponseEntity<Cart> wishUserResponseEntity = restTemplate.exchange(
+                    url,
+                    HttpMethod.DELETE,
+                    null,
+                    Cart.class);
+            return wishUserResponseEntity.getBody();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new ExecutionFailedException(e.getMessage());
+        }
+    }
+
 }
+
